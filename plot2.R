9dda18b5686a811d4646b9d639889576780052a5
cmcwilli <caron.gordon@gmail.com>
2015-06-05 11:24:34 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 ##Plot 2
 df  <- read.table("data/household_power_consumption.txt", header=TRUE,sep=";",stringsAsFactors=FALSE)
 
-subdf <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+subdf <- df[df$Date %in% c("2/2/2007","2/2/2007") ,]
 
 subdf$Global_active_power <- as.numeric(subdf$Global_active_power)
 
